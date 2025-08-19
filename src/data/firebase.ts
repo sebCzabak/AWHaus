b5994dca46f3b0ddd993c155ee043279f5859864
sebCzabak <sczabak@gmail.com>
2025-08-19 15:09:43 +0200

    fix

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -2,8 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
