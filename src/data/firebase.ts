b31e01a7e2a166422f0a391426c9dec4899e28d3
sebCzabak <sczabak@gmail.com>
2025-08-19 15:06:15 +0200

    fix

--- src/data/firebase.ts
+++ src/data/firebase.ts
@@ -3,14 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyAvHhxyet8XgucuuzJHL8DCdZd-RwCZtGQ',
-  authDomain: 'awhaus-strona.firebaseapp.com',
-  projectId: 'awhaus-strona',
-  storageBucket: 'awhaus-strona.firebasestorage.app',
-  messagingSenderId: '539909281156',
-  appId: '1:539909281156:web:e91c0124ae939d6815f9d1',
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
